127771da79d85a5ac0a89f051058ea00c318afb7
Jaemin Choi <jchoi157@login2.stampede2.tacc.utexas.edu>
2019-02-25 13:35:11 -0600

    Fix stampede2 script

--- examples/kronecker/stampede2_scaling.sh
+++ examples/kronecker/stampede2_scaling.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH -J kron-2-s27-e16
-#SBATCH -oe kron.%j
 #SBATCH -p normal
-#SBATCH -N 2
-#SBATCH -n 8
-#SBATCH -t 04:00:00
+#SBATCH -N 16
+#SBATCH -n 64
+#SBATCH -t 01:00:00
+#SBATCH -J kron-16-s25-e16-mpi
+#SBATCH -o %x.stdout
+#SBATCH -e %x.stderr
 
 ulimit -c unlimited
 date
-ibrun /home1/04332/jchoi157/unionfind/examples/kronecker/graph-omp -s 27 -e 16 ++ppn 16 +pemap 0-63 +commap 64-67 +LBOff
+ibrun /home1/04332/jchoi157/unionfind/examples/kronecker/graph-omp -s 25 -e 16 ++ppn 16 +pemap 0-63 +commap 64-67 +LBOff
 date
